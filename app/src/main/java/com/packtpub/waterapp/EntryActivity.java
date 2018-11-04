ef97e519406409c463eb252d712fa691f97a9e14
AJ Sherrell <amiesherrell@gmail.com>
2018-11-04 14:32:12 -0500

    create EntryActivity class, create showEntry() method

---
+++ app/src/main/java/com/packtpub/waterapp/EntryActivity.java
@@ -0,0 +1,14 @@
+package com.packtpub.waterapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EntryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+    }
+
+}
--- app/src/main/java/com/packtpub/waterapp/MainActivity.java
+++ app/src/main/java/com/packtpub/waterapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.packtpub.waterapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -54,4 +55,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private int REQUEST_NEW_ENTRY = 1;
+    private void showEntry() {
+        Intent intent = new Intent(this, EntryActivity.class);
+        startActivityForResult(intent, REQUEST_NEW_ENTRY);
+    }
+
 }
